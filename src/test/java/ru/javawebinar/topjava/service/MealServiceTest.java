277341e0ef227bcb1497fdd24278d84fd67cd6c1
vershinin-fe <few_w@mail.ru>
2017-11-19 00:50:07 +0500

    HW03 Optional MealServiceTest dummy implementation

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,42 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealServiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void getBetweenDateTimes() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void create() throws Exception {
+    }
+
+}
