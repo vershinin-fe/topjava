92b0e80e2e4dcd59c854d8dda0b0d2588e2b07f2
vershinin-fe <few_w@mail.ru>
2017-12-11 02:42:43 +0500

    HW6 Implement meal controllers, Add meals internationalization (HW6 objectives done)

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealController.java
@@ -0,0 +1,84 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@Controller
+public class MealController extends AbstractMealController {
+
+    public MealController(MealService service) {
+        super(service);
+    }
+
+    @GetMapping("/meals")
+    public String meals(Model model) {
+        model.addAttribute("meals", getAll());
+        return "meals";
+    }
+
+    @GetMapping("/deleteMeal")
+    public String deleteMeal(@RequestParam("id") int id) {
+        delete(id);
+        return "redirect:meals";
+    }
+
+    @PostMapping("/meals")
+    public String setFilterBetween(HttpServletRequest request) {
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+
+        request.setAttribute("meals", getBetween(startDate, startTime, endDate, endTime));
+
+        return "meals";
+    }
+
+    @GetMapping("/createMeal")
+    public String toCreateForm(HttpServletRequest request) {
+        final Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        request.setAttribute("meal", meal);
+
+        return "mealForm";
+    }
+
+    @GetMapping("/updateMeal")
+    public String toUpdateForm(@RequestParam("id") int id, HttpServletRequest request) {
+        final Meal meal = get(id);
+        request.setAttribute("meal", meal);
+
+        return "mealForm";
+    }
+
+    @PostMapping("/saveMeal")
+    public String saveMeal(HttpServletRequest request) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        Meal meal = new Meal(
+            LocalDateTime.parse(request.getParameter("dateTime")),
+            request.getParameter("description"),
+            Integer.parseInt(request.getParameter("calories"))
+        );
+
+        if (request.getParameter("id").isEmpty()) {
+            create(meal);
+        } else {
+            update(meal, Integer.parseInt(request.getParameter("id")));
+        }
+
+        return "redirect:meals";
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/user/UserController.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.RootController;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class UserController{
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        AuthorizedUser.setId(userId);
+        return "redirect:meals";
+    }
+}
