a3f62a5b1764182e6b7383745f05b326c675ddb2
vershinin-fe <few_w@mail.ru>
2018-01-01 15:53:10 +0500

    HW8 Meals datatables (excepts ajax)

---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,35 @@
+var ajaxUrl = "ajax/profile/meals/";
+var datatableApi;
+
+$(function () {
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTtime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ]
+    });
+    makeEditable();
+});
